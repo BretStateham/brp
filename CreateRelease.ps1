3c17ef1095aca4706b7795d5a5005cf791cf1314
Bret Stateham <Bret.Stateham@microsoft.com>
2018-03-16 10:59:23 -0700

    Added gitignore to skip all subfolder except TrainingKitPackager

--- CreateRelease.ps1
+++ CreateRelease.ps1
@@ -1,5 +1,11 @@
+# Bret's Release Packager Utility
+# Depends on the TrainingKitPacakger (http://github.com/BretStateham/tkp)
+
+# Modify the $reponame and $repozipurl variables with your repo name and master zip file url
 $reponame = "FlySimExpress"
 $repozipurl = "https://github.com/BretStateham/FlySimExpress/archive/master.zip"
+
+# You do NOT need to modify anything below this point.
 $curloc = Get-Location
 $curpath = $curloc.Path.toString()
 $repounzipfolder = $curpath + "/" + $reponame + "-master"
@@ -14,7 +20,7 @@ Remove-Item -Path $releasezip
 Write-Host "Downloading repo zip file. Please wait..."
 $ProgressPreference = 'SilentlyContinue'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri "https://github.com/BretStateham/FlySimExpress/archive/master.zip" -OutFile $repozip
+Invoke-WebRequest -Uri $repozipurl -OutFile $repozip
 $ProgressPreference = 'Continue'
 Unblock-File -Path $repozip
 Expand-Archive -Path $repozip -Destination $curpath -Force
